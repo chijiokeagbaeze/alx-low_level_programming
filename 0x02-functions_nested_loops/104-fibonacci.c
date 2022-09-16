14766f44be77705e5d64b211e191e7aa859e725a
chijiokeagbaeze <chijiokeagbaeze@gmail.com>
2022-09-15 21:43:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,35 @@
 
 int main(void)
 {
-	int i, fib, fib1 = 1, fib2 = 2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	for (i = 0; i < 97; i++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+	
+	for (i = 1; i < 91; i++)
 	{
-	printf("%d", fib1);
-	while (fib1)
-	{
-	printf(" ,%d", fib2);
-	fib = fib1 + fib2;
-	fib1 = fib2;
-	fib2 = fib;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(" ,%lu", k1 + (k2 / 1000000000));
+		printf("%lu", ku % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 	printf("\n");
+	
 	return (0);
 }
