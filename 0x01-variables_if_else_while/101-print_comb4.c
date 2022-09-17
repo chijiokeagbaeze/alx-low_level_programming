e121e7ea02d82998f27ae24b1dad3a1b2cb29ecc
chijiokeagbaeze <chijiokeagbaeze@gmail.com>
2022-09-17 16:18:19 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all possible combinations of three different digits,
- * in ascending order, 
+ * in ascending order,
  * separated by a comma,
  * followed by a space.
  * Return: 0 always.
