06bc8237e9673eb6560f109c69729d2469abed1d
chijiokeagbaeze <chijiokeagbaeze@gmail.com>
2022-09-23 06:43:03 -0700

    hot fix 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,11 @@ cahr *cap_string(char *n)
 {
 	int i;
 
-	i = 0
-		if (n[0] >= 'a' && n[0] <= 'z')
-		{
-			n[0] = n[0] - 32;
-		}
+	i = 0;
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] = n[0] - 32;
+	}
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		switch (n[i])
