d2e776622542676375c153bb85101b3efb76d3ea
chijiokeagbaeze <chijiokeagbaeze@gmail.com>
2022-09-23 08:40:35 -0700

    hot fix 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -35,7 +35,7 @@ void rev_string(char *n)
  * Return: pointer to calling function.
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size - r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
